45baea7c34fff975b40a613b569b116bcd9279cb
Thercístenes <thercistenes1@outlook.com>
2023-11-14 15:27:19 -0300

    Refatora o título

--- src/components/Imagem/index.tsx
+++ src/components/Imagem/index.tsx
@@ -3,7 +3,7 @@ import react from '../../assets/react.svg';
 function Imagem() {
   return (
     <>
-      <h1>Aplicação usando estados em Context API</h1>
+      <h1>Aplicação usando estados em Context API - React JS</h1>
       <img src={ react } alt="imagem da logo do react" />
     </>
   );
